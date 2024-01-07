a097b263b396d135e422f2bc454711223704ffab
yuyuyu2118 <yuyuyu2118@gmail.com>
2024-01-08 01:54:48 +0900

    refactor/main-title: タイトルを「gin-recipe-hub」に変更

--- main.go
+++ main.go
@@ -21,53 +21,6 @@ type Recipe struct {
 
 var recipes []Recipe
 
-var sampleRecipes = []Recipe{
-	{
-		ID:          "1",
-		Title:       "チキンカレー",
-		Description: "チキンと野菜のカレーです",
-		Ingredients: []string{
-			"玉ねぎ",
-			"にんじん",
-			"じゃがいも",
-			"肉",
-			"カレールー",
-			"水",
-		},
-		Instructions: []string{
-			"玉ねぎ、にんじん、じゃがいもを食べやすい大きさに切る",
-			"鍋にサラダ油を入れて熱し、肉を炒める",
-			"玉ねぎ、にんじん、じゃがいもの順に入れて炒める",
-			"水を入れて具材が浸るまで煮る",
-			"沸騰したらカレールーを入れて溶かす",
-			"好みでカレー粉を入れて味を整える",
-		},
-	},
-	{
-		ID:          "2",
-		Title:       "オムライス",
-		Description: "玉ねぎと卵で作るオムライスです",
-		Ingredients: []string{
-			"玉ねぎ",
-			"卵",
-			"醤油",
-			"ケチャップ",
-			"サラダ油",
-		},
-		Instructions: []string{
-			"玉ねぎを炒める",
-			"卵を溶いて醤油と塩で味付けする",
-			"卵と玉ねぎを混ぜ合わせる",
-			"ご飯の上に卵をかける",
-			"ケチャップをかけて完成",
-		},
-	},
-}
-
-func init() {
-	recipes = sampleRecipes
-}
-
 func main() {
 	// // 環境変数からデータベース接続情報を取得
 	// dbHost := os.Getenv("DB_HOST")
@@ -122,7 +75,7 @@ func main() {
 	// ルートURLにアクセスしたときにindex.htmlをレンダリングする
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", gin.H{
-			"title": "ホームページ",
+			"title": "gin-recipe-hub",
 		})
 	})
 
